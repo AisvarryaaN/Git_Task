387593aaf345a70b20fd4fb17b22e255529fc22a
AisvarryaaN <aisvarryaan@gmail.com>
2025-01-05 12:15:20 +0530

    Added new method to ClassA

--- src/test/java/org/gitdemo/ClassA.java
+++ src/test/java/org/gitdemo/ClassA.java
@@ -14,4 +14,16 @@ public class ClassA {
 		System.out.println("Method 3");
 	}
 
+	private void method7() {
+		System.out.println("Method 7");
+	}
+
+	private void method8() {
+		System.out.println("Method 8");
+	}
+
+	private void method9() {
+		System.out.println("Method 9");
+	}
+
 }
