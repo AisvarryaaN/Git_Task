0f65e8e5961b511192d9c04845c17c78c3f6da45
AisvarryaaN <aisvarryaan@gmail.com>
2025-01-05 11:58:59 +0530

    First push to Master

---
+++ src/test/java/org/gitdemo/ClassA.java
@@ -0,0 +1,17 @@
+package org.gitdemo;
+
+public class ClassA {
+
+	private void method1() {
+		System.out.println("Method 1");
+	}
+
+	private void method2() {
+		System.out.println("Method 2");
+	}
+
+	private void method3() {
+		System.out.println("Method 3");
+	}
+
+}
